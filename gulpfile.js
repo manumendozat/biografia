f064f7a223f7e3cd98f8f4b828f46234c549dce2
erasmo <erasmomontes07@hotmail.com>
2015-03-09 19:01:46 -0400

    Agregando gulp y sus paquetes para automatizar tareas

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var servidor = require('browser-sync');
+var minifycss = require('gulp-minify-css');
+var reload = servidor.reload;
+
+gulp.task('servidor',['css'], function(){
+	servidor({
+		server:{
+			baseDir: "./"
+		}	
+	});
+		gulp.watch('css/*.css', ['css']);
+		gulp.watch('./*.html').on('change', reload);
+	})
+
+gulp.task('css', function(){
+	gulp.src("css/*.css")
+	.pipe(minifycss({keepBreak:true}))
+	.pipe(gulp.dest('mini-css'))
+})
